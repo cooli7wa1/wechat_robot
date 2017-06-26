3f020c400ca9d33fe6c3f5c991c2767605f333f4
liuqi <liuqi@microtrust.com.cn>
2017-06-26 11:13:09 +0800

    add test_x

---
+++ test_x.py
@@ -0,0 +1,24 @@
+#coding:utf-8
+import os, platform, requests, time, pymongo, xlrd, subprocess
+from hashlib import md5
+from requests import RequestException
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException, NoSuchElementException, StaleElementReferenceException
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from lianmeng_config import *
+from multiprocessing import Queue
+
+options = webdriver.ChromeOptions()
+options.add_argument('window-size=1400x900')
+options.add_argument('disable-gpu')
+options.add_argument('window-size=1400x900')
+browser = webdriver.Chrome(chrome_options=options)
+wait = WebDriverWait(browser, 10)
+
+
+browser.get('http://www.baidu.com')
+wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#kw'))).send_keys(u'哈哈')
+browser.get_screenshot_as_png('a.png')
