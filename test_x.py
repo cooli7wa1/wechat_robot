dc26e2af655d3490f81a589c209a94356694114f
liuqi <liuqi@microtrust.com.cn>
2017-06-26 12:49:19 +0800

    a

--- test_x.py
+++ test_x.py
@@ -1,5 +1,5 @@
 #coding:utf-8
-import os, platform, requests, time, pymongo, xlrd, subprocess
+import os, platform, requests, time, pymongo, subprocess
 from hashlib import md5
 from requests import RequestException
 from selenium import webdriver
@@ -8,17 +8,15 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from lianmeng_config import *
-from multiprocessing import Queue
 
 options = webdriver.ChromeOptions()
 options.add_argument('window-size=1400x900')
 options.add_argument('disable-gpu')
-options.add_argument('window-size=1400x900')
+options.add_argument('headless')
 browser = webdriver.Chrome(chrome_options=options)
 wait = WebDriverWait(browser, 10)
 
 
 browser.get('http://www.baidu.com')
 wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#kw'))).send_keys(u'哈哈')
-browser.get_screenshot_as_png('a.png')
+browser.get_screenshot_as_file('a.png')
