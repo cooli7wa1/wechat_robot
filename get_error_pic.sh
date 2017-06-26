47d494297db5a58adbd2f25ab269d9ea33e7602b
liuqi <liuqi@microtrust.com.cn>
2017-06-26 13:56:38 +0800

    a

--- get_error_pic.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-scp root@47.94.156.71:/root/Documents/robot_data/xiaoyezi/pictures/__get_product_from_selection_room_err.png /home/cooli7wa/Downloads/
-
-scp root@47.94.156.71:/root/Documents/robot_data/xiaoyezi/pictures/__login_err.png /home/cooli7wa/Downloads/
